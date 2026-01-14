6a289b1a8642214b5ed2b0286ab9769c8112dd44
Bright Arhin <barhin49@gmail.com>
2026-01-14 09:09:53 +0000

    Remove debug logging for new documents in Navbar component

--- src/Pages/Navbar.jsx
+++ src/Pages/Navbar.jsx
@@ -46,13 +46,6 @@ const Navbar = () => {
     setIsModalOpen(false);
   };
 
-  // Debug: Log new documents
-  useEffect(() => {
-    if (newDocuments && newDocuments.length > 0) {
-      console.log('📬 New documents in Navbar:', newDocuments);
-    }
-  }, [newDocuments]);
-
   useEffect(() => {
     if (term) {
       // console.log(term);
